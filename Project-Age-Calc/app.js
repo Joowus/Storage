cc1e3fc2b2cfc33febae112e8ba0207ec76062f2
Joowus <joona.owusu@gmail.com>
2025-10-23 11:19:09 +0300

    Add files via upload

---
+++ Project-Age-Calc/app.js
@@ -0,0 +1,8 @@
+var name = prompt('What is your name?');
+
+alert('hi ' + name);
+
+var dob = prompt('What year were you born in?');
+var yearTill = prompt('What year till you want to calculate your age?');
+
+alert('You will be ' + (yearTill - dob) + ' years old.');
