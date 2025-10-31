0f1ace6541b2b5f5b2818c01a71c5a1ad1d69d12
Joowus <joona.owusu@gmail.com>
2025-10-31 14:06:55 +0200

    woo

---
+++ Project-Tooltip/app.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () =>{
+
+    const helpTextList = [
+        {
+            id: 'home',
+            text: 'This is the home page where you can find the latest updates.'
+        },        
+        {
+            id: 'about',
+            text: 'This section provides information about our project and team.'
+        },
+        {
+            id: 'contact',
+            text: 'Here you can find our contact details and reach out to us.'
+        }
+    ]
+
+});
